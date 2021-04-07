80dbfdfcb91c60359eb65c1a35a0801d1c7c8baa
Noor Ahmed <nq73399@gmail.com>
2021-04-07 15:06:37 +0500

    first commit

---
+++ src/components/Counter.vue
@@ -0,0 +1,66 @@
+<template>
+<div class="main">
+    <div class="card">
+      <h1>Simple Counter App</h1>
+      <h2>{{count}}</h2>
+      <div class="btncontainer">
+          <Button name="Add" btnClass="btn-primary" @onPress="add" />
+          <Button name="Subtract" btnClass="btn-danger" @onPress="subtract" />
+      </div>
+    </div>
+</div>
+</template>
+
+<script>
+import Button from './button.vue';
+
+export default {
+  name: 'Counter',
+  props: [],
+   components: {
+    Button
+  },
+  data(){
+    return{
+      count: 0
+    }
+  },
+  methods: {
+    add(){
+      this.count = this.count + 1 
+    },
+    subtract(){
+      this.count = this.count - 1  
+    }
+  }
+  }
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+.main{
+  height: 100vh;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+}
+.card{
+  background-color: #cacaca;
+  padding: 20px;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+}
+.btncontainer{
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+  align-items: center;
+}
+
+h1{
+  color: blue;
+}
+</style>
---
+++ src/components/button.vue
@@ -0,0 +1,26 @@
+<template>
+    <button class="btn m-3" :class='btnClass'  @click="onPress">{{name}}</button>
+</template>
+
+<script>
+export default {
+  name: 'Button',
+  props: ['name', 'btnClass'],
+  methods: {
+    onPress(){
+      this.$emit('onPress');
+    }
+  }
+
+ 
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+.shadow{
+    display: flex;
+    justify-content: center;
+    align-items: center;
+}
+</style>
